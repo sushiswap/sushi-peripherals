cb456d4b401577ce41ec2a81d9bf16a56e70f749
0xJiro <jiro@sushi.com>
2023-01-30 01:04:34 -0500

    update server deploy script to deploy boba server

--- script/DeployServer.s.sol
+++ script/DeployServer.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 import "utils/Constants.sol";
 import "chefs/servers/implementations/ArbitrumServer.sol";
 import "chefs/servers/implementations/PosServer.sol";
+import "chefs/servers/implementations/BobaGatewayServer.sol";
 
 contract DeployServer is Script {
   Constants internal constants;
@@ -20,34 +21,15 @@ contract DeployServer is Script {
     // deploys entire set of servers, unless commented out
     deployPolygonServer();
     deployBttcServer();
-
-
+    deployBobaServer();
+    // boba avax
+    // boba bnb
   }
 
 
 
   // indiviudal deployment functions for each deployed server
   // configured for individual network
-  function deployPolygonServer() public {
-    address minichef = constants.getAddress("polygon.minichef");
-    address posManager = 0xA0c68C638235ee32657e8f720a23ceC1bFc77C77;
-    address ercBridge = 0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf;
-    uint256 pid = 344;
-
-    PosServer polygonServer = new PosServer(pid, minichef, posManager, ercBridge);
-    polygonServer.transferOwnership(owner);
-  }
-
-  function deployBttcServer() public {
-    address minichef = constants.getAddress("bttc.minichef");
-    address posManager = 0xD06029b23e9d4CD24bAd01d436837Fa02B8f0dd9;
-    address ercBridge = 0x89a93F94C0a3f388930C4A568430F5e8fFFfd3eC;
-    uint256 pid = 355;
-
-    PosServer bttcServer = new PosServer(pid, minichef, posManager, ercBridge);
-    bttcServer.transferOwnership(owner);
-  }
-
   function deployArbitrumOneServer() public {
     address minichef = constants.getAddress("arbitrum.minichef");
     address bridgeAddr = 0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef;
@@ -66,6 +48,40 @@ contract DeployServer is Script {
     novaServer.transferOwnership(owner);
   }
 
+  function deployBttcServer() public {
+    address minichef = constants.getAddress("bttc.minichef");
+    address posManager = 0xD06029b23e9d4CD24bAd01d436837Fa02B8f0dd9;
+    address ercBridge = 0x89a93F94C0a3f388930C4A568430F5e8fFFfd3eC;
+    uint256 pid = 355;
+
+    PosServer bttcServer = new PosServer(pid, minichef, posManager, ercBridge);
+    bttcServer.transferOwnership(owner);
+  }
+
+  function deployBobaServer() public {
+    address minichef = constants.getAddress("boba.minichef");
+    address bridgeAddr = 0xdc1664458d2f0B6090bEa60A8793A4E66c2F1c00;
+    uint256 pid = 356;
+
+    BobaGatewayServer bobaServer = new BobaGatewayServer(pid, minichef, bridgeAddr, operator);
+    bobaServer.transferOwnership(owner);
+  }
+
+
+
+
+
+
+
+  function deployPolygonServer() public {
+    address minichef = constants.getAddress("polygon.minichef");
+    address posManager = 0xA0c68C638235ee32657e8f720a23ceC1bFc77C77;
+    address ercBridge = 0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf;
+    uint256 pid = 344;
+
+    PosServer polygonServer = new PosServer(pid, minichef, posManager, ercBridge);
+    polygonServer.transferOwnership(owner);
+  }
 
 
 }
